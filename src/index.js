d45eb31465ba4ce3931a1fe69cd30db05980bbb4
Ituriel <80064281+nulledituriel@users.noreply.github.com>
2023-08-28 17:05:35 +0200

    Added index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const config = require('../config.json');
+const { Client, IntentsBitField } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+eventHandler(client);
+
+client.login(config.token);
